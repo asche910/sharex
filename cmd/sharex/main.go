0155a2ee2d09d4878c9c97a2730004af714a5f4e
“asche910” <asche910@gmail.com>
2022-11-16 13:58:01 +0800

    format main.go

--- cmd/sharex/main.go
+++ cmd/sharex/main.go
@@ -10,28 +10,28 @@ import (
 	"strings"
 )
 
-// sharex
+// Application start entry
 func main() {
 	fmt.Println("sharex start")
 
+	// Load config
 	config := conf.InitConfig("sharex.conf")
-
 	fmt.Println("conf:", config)
-	r := gin.Default()
 
+	// Init gin framework
+	r := gin.Default()
 	_ = r.SetTrustedProxies(nil)
 	r.Static("/static", "./web/static")
 
 	var htmlFiles []string
-
 	_ = filepath.WalkDir("web/view", func(path string, d fs.DirEntry, err error) error {
 		if !d.IsDir() && strings.HasSuffix(path, ".html") {
 			htmlFiles = append(htmlFiles, path)
 		}
 		return nil
 	})
-	fmt.Println("Load html files:", htmlFiles)
 	r.LoadHTMLFiles(htmlFiles...)
+	fmt.Println("Load html files:", htmlFiles)
 
 	data := struct {
 		Code int         `json:"code"`
@@ -39,17 +39,16 @@ func main() {
 		Data interface{} `json:"data"`
 	}{200, "success", true}
 
-	//fmt.Println(data)
-
 	r.GET("/", controller.HomeController)
 	r.GET("/download", controller.FileController)
 	r.GET("/img/:name", controller.DownloadController)
-
 	r.GET("/json", func(context *gin.Context) {
 		context.JSON(200, data)
 	})
 
 	TEST()
+
+	// start server
 	fmt.Println("listening: http://localhost:" + config["Port"])
 	_ = r.Run(fmt.Sprintf(":%s", config["Port"]))
 }
